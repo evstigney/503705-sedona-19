432c029aa68503ab65686b322dc1cba748dffcfb
evs <evstigney2010@gmail.com>
2020-05-20 16:54:23 +0300

    подключила интерактивную карту

--- source/js/map.js
+++ source/js/map.js
@@ -1,13 +1,24 @@
-var initMap = function () {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: {lat: 59.936448, lng: 30.321076},
-    disableDefaultUI: true,
+
+ymaps.ready(init);
+function init(){
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [34.862576, -111.793017],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 7
+  })
+
+  var myPlacemark = new ymaps.Placemark([34.862576, -111.793017], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/icon-map-marker.svg',
+    iconImageSize: [30, 30],
+    iconImageOffset: [-15, -15]
   });
 
-  var marker = new google.maps.Marker ({
-    position: {lat: 59.935959, lng: 30.321002},
-    icon: 'img/icon-map-marker.svg',
-    map: map
-  });
+  myMap.geoObjects.add(myPlacemark);
 };
